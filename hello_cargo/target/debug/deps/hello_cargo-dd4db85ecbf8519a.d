4b76187a95675b9df305abd33b74d073df70deac
inazumimakoto <inazumimakoto@gmail.com>
2026-01-01 14:44:21 +0900

    🧹 .gitignore追加 & ビルド成果物を除外

--- hello_cargo/target/debug/deps/hello_cargo-dd4db85ecbf8519a.d
+++
@@ -1,5 +0,0 @@
-/Users/inazumimakoto/Desktop/rust-study/hello_cargo/target/debug/deps/hello_cargo-dd4db85ecbf8519a.d: src/main.rs
-
-/Users/inazumimakoto/Desktop/rust-study/hello_cargo/target/debug/deps/hello_cargo-dd4db85ecbf8519a: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/inazumimakoto/Desktop/rust-study/hello_cargo/target/debug/hello_cargo: /Users/inazumimakoto/Desktop/rust-study/hello_cargo/src/main.rs
