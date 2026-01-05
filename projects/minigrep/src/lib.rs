7d100c2d4cad0bfc7ba899b7121c6bd084c82b3d
inazumimakoto <inazumimakoto@gmail.com>
2026-01-05 17:04:29 +0900

    📝 Ch12: search関数のライフタイム解説（戻り値の参照元はどっち？）

--- projects/minigrep/src/lib.rs
+++ projects/minigrep/src/lib.rs
@@ -1,6 +1,24 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query,contents)
+        );
+    }
+}
 
 pub struct Config {
     pub query: String,
@@ -19,13 +37,25 @@ impl Config {
         Ok(Config { query, filename })
     }
 }
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
+    
+    for line in search(&config.query,&contents) {
+        println!("{}",line);
+    }
 
-    println!("With text:\n{}", contents);
     Ok(())
 }
